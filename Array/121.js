35088ad5a287d36b2d74cda0dec83c8acba7ae9d
SWJ\wangmeng <302740969@qq.com>
2021-03-04 18:54:00 +0800

    121

--- Array/121.js
+++ Array/121.js
@@ -1,9 +1,20 @@
+// 1.暴力解决:遍及
 var maxProfit = function (prices) {
-    let map = new Map()
-    prices.foreach((p, index) => {
-        map.set(index, p)
-    })
-    const p = prices.sort((a, b) => a - b)
+    let l = prices.length;
+    if (l < 2) {
+        return 0;
+    }
 
-    
+    let maxPro = 0; //记录最大利润
+    let min = prices[0]; //记录数组中访问过的最小值
+    for (let i = 1; i < prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        maxPro = Math.max(prices[i] - min, maxPro);
+    }
+    return maxPro
 };
+
+console.log(maxProfit([1, 2]))
+
+
+// 2.动态规划
